c167be60247aa73829f276445e863e13e97e1905
Stefan Martinek <stefan.martinek@atuin.media>
2018-03-16 13:49:55 +0100

    fix(lambda not allowed in static function): moved validator function to directive

--- src/app/ng-currency-formatter/ng-currency-validator.directive.ts
+++ src/app/ng-currency-formatter/ng-currency-validator.directive.ts
@@ -2,6 +2,19 @@ import {Directive, Inject, forwardRef} from '@angular/core';
 import {NG_VALIDATORS, FormControl, Validator} from '@angular/forms';
 import {NgCurrency, NgCurrencyConfig} from './ng-currency';
 
+function validateNumberFactory(config: NgCurrencyConfig) {
+  return (control: FormControl) => {
+    const value: any = control.value;
+    if (NgCurrency.isValid(value) === true && NgCurrency.isNumeric(value) === false) {
+      return {
+        ngCurrencyValidator: {
+          notANumber: config.message
+        }
+      };
+    }
+    return null;
+  };
+}
 
 @Directive({
   selector: '[ngCurrencyValidator][ngModel]',
@@ -18,7 +31,7 @@ export class NgCurrencyValidatorDirective implements Validator {
   validator: Function;
 
   constructor(@Inject('ngCurrencyConfig') config: NgCurrencyConfig) {
-    this.validator = NgCurrency.validateNumberFactory(config);
+    this.validator = validateNumberFactory(config);
   }
 
   validate(c: FormControl) {
--- src/app/ng-currency-formatter/ng-currency.ts
+++ src/app/ng-currency-formatter/ng-currency.ts
@@ -1,27 +1,8 @@
-import {FormControl} from '@angular/forms';
-
 export interface NgCurrencyConfig {
   message: string;
 }
 
 export class NgCurrency {
-
-  static validateNumberFactory(config: NgCurrencyConfig) {
-
-    return (control: FormControl) => {
-      const value: any = control.value;
-      if (NgCurrency.isValid(value) === true && NgCurrency.isNumeric(value) === false) {
-        return {
-          ngCurrencyValidator: {
-            notANumber: config.message
-          }
-        };
-      }
-      return null;
-    };
-
-  }
-
   static isValid(n): boolean {
     return n !== '' && n !== null && n !== undefined;
   }
